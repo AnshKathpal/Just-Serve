11d6e26b7e2317017afa366f62ff1c23afa8eb5c
ASUS <abhiraskar@gmail.com>
2023-07-21 14:23:01 +0530

    day4

--- backend/routes/volunteerRoute.js
+++ backend/routes/volunteerRoute.js
@@ -16,53 +16,6 @@ volunteerRouter.post("/add", async (req, res) => {
   }
 });
 
-// get
-volunteerRouter.get("/get", async (req, res) => {
-  let { location, type, page, limit } = req.query;
-
-  try {
-    if (location && type) {
-      const data = await VolunteerModel.find({
-        $and: [{ location: location }, { typeofwork: type }],
-      });
-      res.status(200).json({ data: data });
-    } else if (location) {
-      const data = await VolunteerModel.find({ location: location });
-      res.status(200).json({ data: data });
-    } else if (type) {
-      const data = await VolunteerModel.find({ typeofwork: type });
-      res.status(200).json({ data: data });
-
-    } else if (page) {
-      page = parseInt(page) || 1; // Default to page 1 if page parameter is not provided
-      limit = parseInt(limit) || 10; // Default to 10 items per page if limit parameter is not provided
-      try {
-        const totalBooks = await VolunteerModel.countDocuments();
-        const totalPages = Math.ceil(totalBooks / limit);
-
-        const books = await VolunteerModel.find()
-          .skip((page - 1) * limit)
-          .limit(limit);
-
-        res.json({
-          currentPage: page,
-          totalPages,
-          data: books,
-        });
-      } catch (err) {
-        res.status(500).json({ error: "Failed to fetch books" });
-      }
-
-    } else {
-      const data = await VolunteerModel.find({});
-      res.status(200).json({ data: data });
-    }
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
-
-
 
 // update
 volunteerRouter.patch("/update/:id", async (req, res) => {
